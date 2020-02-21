bdc92ca73d51e7cfd1a2e98ad6c017ce6f91747f
Kallum Panditharatna <kallum179@gmail.com>
2020-02-21 11:31:44 +0000

    30Days

---
+++ 30Days/HackerRank_Day6/HackerRank_Day6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HackerRank_Day6 {
+    class Program {
+        static void Main(string[] args) {
+
+            
+            //used to store how many strings will be entered
+            int noStrings = Convert.ToInt32(Console.ReadLine());
+
+            //an array of all the inputs
+            string[] inputs = new string[noStrings];
+
+
+            //loops through the amount of strings
+            for (int i = 0; i < noStrings; i++)
+
+                //takes each string input
+                inputs[i] = Console.ReadLine();
+            
+
+            //loops through the amount of strings
+            for (int i = 0; i < noStrings; i++) {
+
+                //loops through each even letter in the current string
+                for (int j = 0; j < inputs[i].Length; j+= 2) {
+
+                    //outputs each of the 
+                    Console.Write(inputs[i].Substring(j, 1));
+
+
+                }
+                Console.Write(" ");
+                //loops through each odd letter in the current string
+                for (int j = 1; j < inputs[i].Length; j += 2) {
+
+                    //outputs each of the 
+                    Console.Write(inputs[i].Substring(j, 1));
+
+
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
